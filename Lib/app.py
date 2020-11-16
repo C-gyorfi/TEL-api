7917d36e782f6e8b249ff8d3a75c7a5f2749152e
csabagyorfi <csaba@madetech.com>
2020-11-16 13:32:27 +0000

    Setup local DB for dev and testing

--- Lib/app.py
+++ Lib/app.py
@@ -1,6 +1,14 @@
-from flask import Flask 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
--- Test/test_can_request_a_list_of_food_items.py
+++ Test/test_can_request_a_list_of_food_items.py
@@ -1,12 +1,22 @@
 import pytest
 from assertpy import assert_that
-from Lib import app
+from flask_sqlalchemy import SQLAlchemy
+from Lib.app import app
+import os
 
 # Given there are food items in a list, (Setup the database with 3 Food items)
 # When I call the list food items endpoint with a list ID
 # Then I want to be able to see the three food items
 
+@pytest.fixture(scope="session", autouse=True)
+def setup(request):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('TEST_DATABASE_URL')
+    db = SQLAlchemy(app)
+    db.drop_all()
+    db.create_all()
+
 def test_request_root():
-    response = app.app.test_client().get('/')
+    response = app.test_client().get('/')
     assert_that(response.status_code).is_equal_to(200)
     assert_that(response.data).contains(b'Hello, have you saved food today?')
+    assert_that(app.config["SQLALCHEMY_DATABASE_URI"]).contains('test.db')
