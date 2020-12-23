f69d8281edf6f3d91f35b073a2d759fdd50c7562
csabagyorfi <csaba@madetech.com>
2020-12-23 11:15:14 +0000

    Update CORS policy to allow DELETE

--- Lib/app_factory.py
+++ Lib/app_factory.py
@@ -3,7 +3,7 @@ import os
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": os.getenv('ALLOWED_ORIGINS')}})
+CORS(app, resources={r"/api/*": {"origins": os.getenv('ALLOWED_ORIGINS')}}, methods=["GET", "POST", "DELETE"])
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
